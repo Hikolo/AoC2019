d2f1344997c98fccea13e9b34986b45fe8b5fb4b
Hikolo <Hikolo.92@gmail.com>
2020-01-01 22:06:01 +0100

    Add elixir d1 p2

--- day1/part1.ex
+++ day1/part1.ex
@@ -4,17 +4,49 @@ defmodule PartOne do
   end
 
   def addMass([], acc), do: acc
-  def addMass([ h| t ], acc), do: addMass(t, acc + calculateMass(String.to_integer(h)))
+  def addMass([ mass | tail ], acc), do: addMass(tail, acc + calculateMass(String.to_integer(mass)))
 
   def part1(input) do
     addMass(readInput(input), 0)
   end
+
   def readInput(file_name) do
     case File.read(file_name) do
       {:ok, file} ->
 	String.split(file, "\n")
       _ ->
 	IO.puts "Couldn't read file"
+    end
+  end
+end
+
+defmodule PartTwo do
+  def calculateMass(m) do
+    fuel = div(m, 3) - 2
+    calculateMass(fuel, fuel)
+  end
+  def calculateMass(m, acc) do
+    fuel = div(m, 3) - 2
+    if (fuel > 0) do
+      calculateMass(fuel, acc + fuel)
+    else
+      acc
+    end
+  end
+
+  def addMass([], acc), do: acc
+  def addMass([ mass | tail ], acc), do: addMass(tail, acc + calculateMass(String.to_integer(mass)))
+
+  def part2(input) do
+    addMass(readInput(input), 0)
+  end
+
+  def readInput(file_name) do
+    case File.read(file_name) do
+      {:ok, file} ->
+  String.split(file, "\n")
+      _ ->
+  IO.puts "Couldn't read file"
     end
   end
 end
